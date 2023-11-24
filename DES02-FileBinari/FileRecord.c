ad7a4563b0b983f496681bf8e0552f4a41cb9a9c
lorenzo745 <lorenzo.zanirato6@gmail.com>
2023-11-24 09:56:56 +0000

    commit

--- DES02-FileBinari/FileRecord.c
+++ DES02-FileBinari/FileRecord.c
@@ -60,7 +60,7 @@ int main()
 {
 	int r;
     srand(time(NULL));
-    carica("studenti.dat");
+    //carica("studenti.dat");
     stampa("studenti.dat");
     //r = cercacognome("studenti.dat", "zanirato");       //funziona
     //printf("\nil cognome zanirato e' stato inserito: %d\n",r);
