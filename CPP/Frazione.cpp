fec3247ebb31854b991214adc2dfb44b1c8f361d
lorenzo745 <lorenzo.zanirato6@gmail.com>
2024-01-10 12:46:52 +0000

    commit

---
+++ CPP/Frazione.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+
+using namespace std;
+
+class Frazione {
+    private:
+        int numeratore;
+        int denominatore;
+
+    public:
+        Frazione () {}; //costuttore
+        void setnumeratore(int n)
+        {
+            numeratore=n;
+        }
+        void setdenominatore(int d)
+        {
+            denominatore=d;
+        }
+
+        int getnumeratore()
+        {
+            return numeratore;
+        }
+
+        int getdeniminatore()
+        {
+            return denominatore;
+        }
+        
+        void stampa()
+        {
+            cout<< numeratore << "/" << denominatore << endl;
+        }
+        void semplifica ()
+        {
+            int min;
+            min=MCD(numeratore,denominatore);
+            numeratore/=min;
+            denominatore/=min;
+        }
+
+        int MCD (int a,int b)
+        {
+	    int r=0;
+	    while(b!=0)
+	    {
+		    r=a%b;
+		    a=b;
+		    b=r;
+        } 
+	    return a;	
+        }   
+};
+
+int main ()
+{
+    Frazione f1;
+    int n,d;
+    cout<<"inserisci prima il numeratore e poi il deniminatore"<<endl;
+    cin>>n;
+    cin>>d;
+    cout<<"\n"<<endl;
+    f1.setnumeratore(n);
+    f1.setdenominatore(d);
+    f1.stampa();
+
+    f1.semplifica();
+    f1.stampa();
+
+}
