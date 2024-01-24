2354457e03556d35f0e065a418b5ba1f1c9a0529
lorenzo745 <lorenzo.zanirato6@gmail.com>
2024-01-24 12:17:00 +0000

    commit

--- RETTANGOLO/main.cpp
+++ RETTANGOLO/main.cpp
@@ -3,8 +3,8 @@
 int main()
 {
     Rettangolo r1;
-    Rettangolo r2;
-    Rettangolo r3;
+    Rettangolo r2(5);
+    Rettangolo r3(10,5);
     r1.setBase(12);
     r1.setAltezza(8);
     r1.stampaTutto();
