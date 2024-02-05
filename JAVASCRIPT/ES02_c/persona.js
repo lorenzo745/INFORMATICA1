a592f55201728627b8153b2f2506682e780685a1
lorenzo745 <lorenzo.zanirato6@gmail.com>
2024-02-05 08:39:09 +0000

    commit

--- JAVASCRIPT/ES02_c/persona.js
+++ JAVASCRIPT/ES02_c/persona.js
@@ -4,4 +4,6 @@ persona.nome = prompt("Inserisci il nome")
 persona.cognome = prompt("Inserisci il cognome")
 persona.eta = prompt("Inserisci l'eta")
 
-console.log(persona)
+console.log("nome = " + persona.nome)
+console.log("cognome = " + persona.cognome)
+console.log("eta'= " + persona.eta)
