42440e77b2735ef5fc80869bf12db5913763af4f
lorenzo745 <lorenzo.zanirato6@gmail.com>
2024-02-08 15:15:49 +0000

    commit

--- CPP/ES03_CONTATORE/Contatore.cpp
+++ CPP/ES03_CONTATORE/Contatore.cpp
@@ -1,72 +1,71 @@
 #include<iostream>
-#include<cmath>
+#include<math.h>
+using namespace std;
 #include "Contatore.h"
 
-using namespace std;
+    Contatore::Contatore(){
 
-Contatore::Contatore()
-{
-    this->valore=0;
-    this->passo=1;
-}
-Contatore::Contatore(int valore)
-{
-    this->valore=valore;
-    this->passo=1;
-}
-Contatore::Contatore(int valore, int passo)
-{
-    this->valore=valore;
-    this->passo=passo;
-}
-
-void Contatore::setValore(int valore)
-{
-    this->valore=valore;
-} 
-int Contatore::getValore()
-{
-    return valore;
-}
-void Contatore::setPasso(int passo)
-{
-    this->passo=passo;
-}
-
-int Contatore::getPasso()
-{
-        return passo;
-}
-void Contatore::reset()
-{
-    this valore=0;
-}
-void Contatore::inc()
-{
-    
-}
-double Rettangolo::calcolaDiagonale()
-{
-    return sqrt(base*base+altezza*altezza);
-}
-void Rettangolo::stampaTutto()
-{
-    cout<<"base "<<base<<endl;
-    cout<<"altezza "<<altezza<<endl;
-    cout<<"perimetro "<<calcolaPerimetro()<<endl;
-    cout<<"area "<<calcolaArea()<<endl;
-    cout<<"diagonale "<<calcolaDiagonale()<<endl;
-}
-
-
-
-
-int Contatore::media()
-{
-    int v[N];
-    for(int i=0;i<N;i++)
-    {
-        cout<<"base "<<base<<endl;
-        cin>>v[i];
+        this->valore=0;
+        this->passo=1;
     }
-}
+    Contatore::Contatore(int valore){
+
+        this->valore=valore;
+
+    }
+    Contatore::Contatore(int valore,int passo){
+
+        this->passo=passo;
+    }
+
+    //~Contatore();  /*distruttore*/
+
+    void Contatore::setValore(int valore){
+
+        this->valore=valore;
+    }
+
+    int Contatore::getValore(){
+
+        return valore;
+
+    }
+
+    void Contatore::setPasso(int passo){
+
+        this->passo=passo;
+
+    }
+    int Contatore::getPasso(){
+
+        return passo;
+
+    }
+
+    void Contatore::reset(){
+
+        this->valore=0;
+    }
+
+    void Contatore::inc(){
+
+        for(int i=0;i < passo;i++){
+
+            this->valore+=valore;
+        }
+
+    }
+
+    void Contatore::dec(){
+
+        for(int i=0;i < passo;i--){
+
+            this->valore=valore-1;
+        }
+    }
+
+    void Contatore::stampa(){
+
+        cout<<"il valore e': "<<valore<<endl;
+        cout<<"il passo e': "<<passo<<endl;
+    }
