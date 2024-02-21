cc925dcbfad32b4092609665d96974b0c75d3e28
lorenzo745 <lorenzo.zanirato6@gmail.com>
2024-02-21 13:07:38 +0000

    commit

---
+++ JAVASCRIPT/ES03_a/a.js
@@ -0,0 +1,5 @@
+for(let i=100;i>=0;i-=20)
+{
+    console.log(i);
+
+}
---
+++ JAVASCRIPT/ES03_b/b.js
@@ -0,0 +1,10 @@
+libro={
+titolo:"le cronache di narnia",
+autore:"C. S. Lewis",
+data:"30/02/24",
+genere:"horror",
+pagine:"1000"
+}
+
+for(i in libro)
+    console.log(`${i}= ${libro[i]}`)
