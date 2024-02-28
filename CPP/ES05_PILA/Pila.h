444488e4656e8944f377fc4a66a9fbd90dbdc620
lorenzo745 <lorenzo.zanirato6@gmail.com>
2024-02-28 12:14:58 +0000

    commit

---
+++ CPP/ES05_PILA/Pila.h
@@ -0,0 +1,20 @@
+ #ifdef PILA_H
+ #define PILA_H
+
+ class Pila 
+{
+    private:
+        int *vet;
+        int size;
+        int fine;
+    public:
+        Pila();
+        Pila(int);
+        bool isEmpty();
+        bool isFull();
+        bool push(int);
+        bool pop(void);
+        int top(void);
+        void stampa(void);
+};
+#endif
